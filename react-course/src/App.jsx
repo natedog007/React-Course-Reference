fed0aa0727902929206923ff67f75ebf730d50be
Nathan Boyden <nrb121903@gmail.com>
2025-07-15 17:50:11 -0400

    some stuff

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -1,13 +1,73 @@
+import Search from "./components/Search";
+import { useState, useEffect } from "react";
 
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'Get',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  
+  const fetchMovies = async () => {
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+  
+      if(!response.ok){
+        throw new Error("Error");
+      }
+
+      const data = await response.json();
+      console.log(data)
+
+      if (data.response == false){
+      setErrorMessage(data.Error || "Failed to Fetch Movies");
+      }
+
+    }
+    catch(error){
+      console.error(`Fetching error: ${error}`);
+      setErrorMessage('Error Fetching Movies.')
+    }
+  }
+
+
+  useEffect(() => {
+    fetchMovies();
+  }, [])
 
   return (
-    <>
-      <h1>Hello Guys</h1>
-      <h2>Hello</h2>
-    </>
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm}/>
+        </header>
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
+
+        
+      </div>
+    </main>
+
+
   )
 }
 
-export default App
+export default App;
---
+++ react-course/src/components/Search.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+
+function Search({ searchTerm, setSearchTerm }) {
+    return(
+        <div className="search">
+            <div>
+                <img src="./search.svg"/>
+
+                <input
+                    type="text"
+                    placeholder="Search"
+                    value = {searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                />
+            </div>
+        </div>
+    ) 
+}
+
+
+export default Search;
