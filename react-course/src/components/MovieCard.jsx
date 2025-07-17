c42f7d0508725dd3542e77c2f68d9da89d037292
Nathan Boyden <nrb121903@gmail.com>
2025-07-17 00:41:34 -0400

    added some images

--- react-course/src/components/MovieCard.jsx
+++ react-course/src/components/MovieCard.jsx
@@ -1,7 +1,8 @@
 
-function MovieCard() {
+function MovieCard({ movie: {title , id, vote_average, poster_path, release_date, original_language } }) {
     return(
-        <div>
+        <div className="movie-card">
+            <img src={poster_path ? `http://image.tmdb.org/t/p/w500/${poster_path}` : 'no/movie/.png'}/>
         </div>
     )
 }
